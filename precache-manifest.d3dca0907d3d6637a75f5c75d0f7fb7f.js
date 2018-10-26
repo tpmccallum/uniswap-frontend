18a4959856494d946fc030429d19dd4469322d34
Hayden Adams <haydenzadams@gmail.com>
2018-10-25 22:38:48 -0400

    Updates

---
+++ precache-manifest.d3dca0907d3d6637a75f5c75d0f7fb7f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "./static/media/metamask-logo.023762b6.png"
+  },
+  {
+    "revision": "5f4be515aa6c0a3b7ec4",
+    "url": "./static/css/main.42557609.chunk.css"
+  },
+  {
+    "revision": "689e582abbd5c069ddb9",
+    "url": "./static/js/1.689e582a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cd061363bbf9cd7a26cb09b642dcaf63",
+    "url": "./static/media/arrow-down-blue.cd061363.svg"
+  },
+  {
+    "revision": "0c1b02af8afb13c4be8574e42e15b64a",
+    "url": "./static/media/pending.0c1b02af.svg"
+  },
+  {
+    "revision": "0be980e8395d5fde00ee3c8165d0c15a",
+    "url": "./static/media/trust-wallet-logo.0be980e8.svg"
+  },
+  {
+    "revision": "cb563c38afade37f55d04f2cd853d167",
+    "url": "./static/media/brave-logo.cb563c38.png"
+  },
+  {
+    "revision": "5f4be515aa6c0a3b7ec4",
+    "url": "./static/js/main.5f4be515.chunk.js"
+  },
+  {
+    "revision": "040d3851a083aab7f00a01aa0ddd023b",
+    "url": "./static/media/ethereum-logo.040d3851.png"
+  },
+  {
+    "revision": "c73674bb0d585b4172cd0d8c15c5af34",
+    "url": "./static/media/generic-token-logo.c73674bb.png"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "./static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "c89be69c2593e3de93f102d510b814a9",
+    "url": "./static/media/qr-code.c89be69c.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "./static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "b20914ec5482543a0b1b2c6d5509ab96",
+    "url": "./static/media/dropdown-blue.b20914ec.svg"
+  },
+  {
+    "revision": "a57b6a6ec9e849d567c12f0c4f75f890",
+    "url": "./index.html"
+  }
+];

6c195f9b7e01fe810205f81be2b1c5e3df7c174a
maggie-5miles <maggie@wespoke.com>
2019-03-01 10:46:09 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a48ee10a383071df812518237960827f.js"
+  "./precache-manifest.3bb2bd27605155641652f067e3e6a75d.js"
 );
 
 workbox.clientsClaim();
